2aa3210d9379e44ec5947d38cd9d5334dddb7a05
joy-2711 <joyleen0509.becse24@chitkara.edu.in>
2025-05-04 19:14:44 +0530

    added an indian recipe

--- website/finder.js
+++ website/finder.js
@@ -584,8 +584,16 @@ const recipes = [
           "type": "Non-Vegetarian",
           "image": "https://images.themodernproper.com/production/posts/2019/Chicken-Picatta-8.jpg?w=800&q=82&auto=format&fit=crop&dm=1689343305&s=51c93f70d2c9eb342fe7862c7b752d08",
           "description": "Chicken Piccata is a classic Italian-American dish featuring tender chicken breasts lightly dredged in flour and pan-fried, then simmered in a tangy lemon-butter sauce with capers and white wine. It's bright, savory, and perfect with pasta or vegetables."
-        }
-      
+        },
+        {
+            "name": "Shahi Paneer",
+            "ingredients": ["paneer", "onion", "tomato", "cashew nuts", "cream", "butter", "spices"],
+            "cuisine": "Indian",
+            "type": "Vegetarian",
+            "image": "https://www.indianhealthyrecipes.com/wp-content/uploads/2021/07/shahi-paneer-recipe.jpg",
+            "description": "A royal North Indian curry made with paneer cubes in a rich, creamy, and mildly spiced gravy of onions, tomatoes, and cashews."
+          },
+          
       
   
     
